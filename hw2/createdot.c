ec6015627243496c4bd8e7585e480853c319ada1
Caitlin Ross <rossc3@rpi.edu>
2016-02-01 14:41:02 -0500

    finished data parsing

--- hw2/createdot.c
+++ hw2/createdot.c
@@ -2,22 +2,29 @@
 #include <stdio.h>
 #include "glib.h"
 
-#define MAX_PARTS 6
+#define MAX_PARTS 5
+#define NUM_ENTRIES 28
 #define MAX_LENGTH 1024
 
-void parse_data(char *filename);
+void parse_data(char *filename, int **data);
 
 int main(int argc, char **argv)
 {
     char filename[256];
     sprintf(filename, "data.txt");
 
-    parse_data(filename);
+    // parse data from file
+    int **data = malloc(NUM_ENTRIES*sizeof(int*));
+    for (int i = 0; i < NUM_ENTRIES; i++){
+        data[i] = malloc(MAX_PARTS*sizeof(int));
+    }
+    parse_data(filename, data);
 
     return EXIT_SUCCESS;
 }
 
-void parse_data(char *filename)
+/* parses the data from the file and stores in 2d array */
+void parse_data(char *filename, int **data)
 {
     FILE *f = fopen(filename, "r");
     char line[MAX_LENGTH];
@@ -26,14 +33,19 @@ void parse_data(char *filename)
         exit(EXIT_FAILURE);
     }
 
+    int i = 0;
     while (fgets(line, sizeof(line), f) != NULL) /* read a line */
     {
-        gchar **parts = g_strsplit(line, ",", MAX_PARTS);
-        for (int i = 0; i < MAX_PARTS; i++)
+        if (line[0] == '#')
         {
-
+            gchar **parts = g_strsplit(line, ",", MAX_PARTS);
+            for (int j = 0; j < MAX_PARTS; j++)
+            {
+                    data[i][j] = atoi(parts[j]);
+            }
+            i++;
+            g_strfreev(parts);
         }
-        g_strfreev(parts);
     }
 
 }
