5c5a840031717c77b23f802681abccb6621973f1
Caitlin Ross <rossc3@rpi.edu>
2016-02-03 16:49:18 -0500

    adding awesim graph, some changes to cmd line opts

--- hw2/createdot.c
+++ hw2/createdot.c
@@ -8,38 +8,45 @@
 #define NUM_ENTRIES 28
 #define MAX_LENGTH 1024
 
-static void parse_data(char *filename, int **data);
+//static void parse_data(char *filename, int **data);
+void parse_data(char *filename, int **data);
+void class_data_stuff(char *filename);
 static int parse_opt(int key, char *arg, struct argp_state *state);
-void graphing(int **data, char *name, Agdesc_t type);
-static Agraph_t *create_graph(char *name, Agdesc_t type, int **data);
+static void graphing(int **data, char *name, Agdesc_t type);
+static Agraph_t *class_graph(char *name, Agdesc_t type, int **data);
+static Agraph_t *awesim_inf_graph(char *name, Agdesc_t type);
 
 char doc[] = "This program generates DOT files for graphvis";
 char args_doc[] = "";
 
-int num_nodes = 0;
+int num_nodes = 5;
+int awesim_f = 0;
+int class_f = 0;
+char *filename;
+Agdesc_t type;
 
 static struct argp_option options[] = {
-    {"nodes", 'n', "NUM", 0, "number of nodes in graph"},
+    {"nodes", 'n', "n", 0, "number of nodes in graph"},
     {"sparse", 's', 0, 0, "sparse graph"},
     {"type", 't', 0, 0, "type of graph"},
+    {"awesim", 'a', 0, 0, "create graph of awesim infrastructure"},
+    {"class-data", 'c', "str", 0, "input filename to create graph from class data"},
+    {"bipartite", 'b', 0, 0, "create a bipartite graph (use --nodes to set size)"},
+    {"undirected", 'd', 0, 0, "set to make graph undirected, default is directed"},
     {0}
 };
 static struct argp argp = {options, parse_opt, args_doc, doc};
 
 int main(int argc, char **argv)
 {
+    type = Agstrictdirected;
+    filename = malloc(sizeof(char)*256);
     argp_parse(&argp, argc, argv, 0, 0, 0);
-    char filename[256];
-    sprintf(filename, "data.txt");
 
-    // parse data from file
-    int **data = malloc(NUM_ENTRIES*sizeof(int*));
-    for (int i = 0; i < NUM_ENTRIES; i++){
-        data[i] = malloc(MAX_PARTS*sizeof(int));
-    }
-    parse_data(filename, data);
-
-    graphing(data, "class", Agundirected);
+    if (awesim_f)
+        graphing(NULL, "awesim", type);
+    if(class_f)
+        class_data_stuff(filename);
 
     return EXIT_SUCCESS;
 }
@@ -68,7 +75,17 @@ void parse_data(char *filename, int **data)
             g_strfreev(parts);
         }
     }
+}
 
+void class_data_stuff(char *filename)
+{
+    int **data = malloc(NUM_ENTRIES*sizeof(int*));
+    for (int i = 0; i < NUM_ENTRIES; i++){
+        data[i] = malloc(MAX_PARTS*sizeof(int));
+    }
+    parse_data(filename, data);
+
+    graphing(data, "class", Agdirected);
 }
 
 /* used by argp to parse command line args */
@@ -79,6 +96,16 @@ int parse_opt(int key, char *arg, struct argp_state *state)
         case 'n':
             num_nodes = atoi(arg);
             break;
+        case 'a':
+            awesim_f = 1;
+            break;
+        case 'c':
+            class_f = 1;
+            strcpy(filename, arg);
+            break;
+        case 'd':
+            type = Agstrictundirected;        
+
     }
     return 0;
 }
@@ -89,8 +116,16 @@ void graphing(int **data, char *name, Agdesc_t type)
     Agraph_t *G;
     GVC_t *gvc;
     gvc = gvContext();
-    G = create_graph(name, type, data);
-    gvLayout(gvc, G, "circo");
+    if (strcmp(name, "class") == 0)
+    {
+        G = class_graph(name, type, data);
+        gvLayout(gvc, G, "circo");
+    }
+    else //if (strcmp(name, "awesim") == 0)
+    {
+        G = awesim_inf_graph(name, type);
+        gvLayout(gvc, G, "dot");
+    }
     char filename[256];
     sprintf(filename, "%s.gv", name);
     gvRenderFilename(gvc, G, "dot", filename);
@@ -101,7 +136,7 @@ void graphing(int **data, char *name, Agdesc_t type)
     gvFreeContext(gvc);
 }
 
-Agraph_t *create_graph(char *name, Agdesc_t type, int **data)
+Agraph_t *class_graph(char *name, Agdesc_t type, int **data)
 {
     Agraph_t *G = agopen(name, type, 0);
     Agnode_t *root = agnode(G, "Interactive Vis Students", 1);
@@ -176,3 +211,53 @@ Agraph_t *create_graph(char *name, Agdesc_t type, int **data)
     return G;
 }
 
+Agraph_t *awesim_inf_graph(char *name, Agdesc_t type)
+{
+    Agraph_t *G = agopen(name, type, 0);
+    Agnode_t *awe_serv = agnode(G, "AWE Server", 1);
+    Agraph_t *site1 = agsubg(G, "cluster_wan1", 1);
+    agset(site1, "label", "WAN Site 1");
+    agset(site1, "concentrate", "true");
+    Agnode_t *shock = agnode(site1, "Shock data server", 1);
+    Agnode_t *router = agnode(site1, "router", 1);
+    agedge(site1, shock, router, NULL, 1);
+    agedge(site1, router, shock, NULL, 1);
+    Agraph_t *site2 = agsubg(G, "cluster_wan2", 1);
+    agset(site2, "label", "WAN Site 2");
+
+    Agnode_t *proxy1 = agnode(site1, "Proxy1", 1);
+    agset(proxy1, "label", "Proxy");
+    Agnode_t *proxy2 = agnode(site2, "Proxy2", 1);
+    agset(proxy2, "label", "Proxy");
+
+    agedge(site1, router, proxy1, NULL, 1);
+    agedge(site1, proxy1, router, NULL, 1);
+    agedge(G, router, proxy2, NULL, 1);
+    agedge(G, proxy2, router, NULL, 1);
+
+    for (int i = 0; i < num_nodes; i++)
+    {
+        char tmp[16];
+        sprintf(tmp, "local client %d", i);
+        Agnode_t *c1 = agnode(site1, tmp, 1);
+        agset(c1, "label", "Client");
+        agedge(site1, proxy1, c1, NULL, 1);
+        agedge(site1, c1, proxy1, NULL, 1);
+        agedge(G, awe_serv, c1, NULL, 1);
+        agedge(G, c1, awe_serv, NULL, 1);
+
+        sprintf(tmp, "remote client %d", i);
+        Agnode_t *c2 = agnode(site2, tmp, 1);
+        agset(c2, "label", "Client");
+        agedge(site2, proxy2, c2, NULL, 1);
+        agedge(site2, c2, proxy2, NULL, 1);
+        agedge(G, awe_serv, c2, NULL, 1);
+        agedge(G, c2, awe_serv, NULL, 1);
+    }
+    
+    return G;
+}
+
+Agraph_t *bipartite_graph(char *name, Agdesc_t type)
+{
+}
